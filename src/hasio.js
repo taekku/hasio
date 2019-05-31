8b15c585b5790309ae9c0a8ca994bf236be73928
Taekku Lim <taekku.lim@gmail.com>
2019-05-31 15:50:29 +0900

    Hello Babel

--- src/hasio.js
+++ src/hasio.js
@@ -1,1 +1,9 @@
+"use strict";
+import { Jumin } from './hasio/common/type/jumin.js';
+
+let jumin = new Jumin('asdf');
+
 console.log("Hello World!");
+console.log(jumin);
+console.log('my SSN:' + jumin.getSSN());
+console.log('gender:' + jumin.gender());
---
+++ src/hasio/common/type/jumin.js
@@ -0,0 +1,12 @@
+"use strict";
+export class Jumin{
+  constructor(ssn){
+    this.ssn = ssn;
+  }
+  getSSN(){
+    return this.ssn;
+  }
+  gender(){
+    return '1';
+  }
+}

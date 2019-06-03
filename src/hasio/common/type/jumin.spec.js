3ece18d33809a4cc5d214488cf900af6efca7563
T.G. Lim <taekgu@gmail.com>
2019-06-04 00:58:25 +0900

    Karma+Jasmine

---
+++ src/hasio/common/type/jumin.spec.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("Jumin", function () {
+  it("contains spec with an expectation", function () {
+    expect(true).toBe(true);
+  });
+});
---
+++ test/hasio.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+  describe("Hasio", ()=> {
+    it("contains spec with an expectation", ()=> {
+      expect(true).toBe(true);
+    })
+  });

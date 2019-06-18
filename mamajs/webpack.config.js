db06626dd3133cbde3a63230a4509706f3171ed7
T.G. Lim <taekgu@gmail.com>
2019-06-19 00:16:22 +0900

    monaco editor

--- mamajs/webpack.config.js
+++ mamajs/webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // 'production', 'development', 'none'
   // enntry file
-  entry: ['./src/hasio.js'],
+  entry: {
+    "hasio" : './src/hasio.js',
+    "monaco" : './src/monaco.js',
+		"editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
+		"json.worker": 'monaco-editor/esm/vs/language/json/json.worker',
+		"css.worker": 'monaco-editor/esm/vs/language/css/css.worker',
+		"html.worker": 'monaco-editor/esm/vs/language/html/html.worker',
+		"ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker',
+  },
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+		globalObject: 'self',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/dist'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/i,
         include: [
           path.resolve(__dirname, 'src')
         ],
@@ -23,6 +34,10 @@ module.exports = {
            // plugins: ['@babel/plugin-proposal-class-properties']
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader'],
       }
     ]
   },

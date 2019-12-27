71dc176130643023a7987025abdd058e1a0a2cad
임택구 <taekgu@gmail.com>
2019-12-27 17:46:13 +0900

    pong

--- pong/src/main/java/com/goldgaram/pong/PongApplication.java
+++ pong/src/main/java/com/goldgaram/pong/PongApplication.java
@@ -2,9 +2,15 @@ package com.goldgaram.pong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PongApplication {
+public class PongApplication extends SpringBootServletInitializer {
+
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(PongApplication.class);
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PongApplication.class, args);
